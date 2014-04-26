6977c75898e7870abc9c32960b17aed52f440023
Gengar003 <gengar003@gmail.com>
2014-04-26 02:17:44 -0500

    squash me

--- test.rb
+++ test.rb
@@ -14,7 +14,7 @@ GITHUB_ORG = params["org"]
 GITHUB_REPO = params["repo"]
 GITHUB_SRCROOT = "/src"
 GITHUB_PROPERTIES = "#{GITHUB_URL}/#{GITHUB_ORG}/#{GITHUB_REPO}/master/properties/defaults.properties"
-
+puts "properties at #{GITHUB_PROPERTIES}<hr>"
 demo = Includer.new(
 	GITHUB_URL,
 	GITHUB_ORG,
