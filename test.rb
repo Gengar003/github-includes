e83058d9ca6ef8d6b5a49101eb72684437f02d46
Gengar003 <gengar003@gmail.com>
2014-04-26 03:12:21 -0500

    [updated] also takes propert path on the command line

--- test.rb
+++ test.rb
@@ -13,7 +13,8 @@ GITHUB_URL = "https://raw.githubusercontent.com"
 GITHUB_ORG = params["org"]
 GITHUB_REPO = params["repo"]
 GITHUB_SRCROOT = "src"
-GITHUB_PROPERTIES = "#{GITHUB_URL}/#{GITHUB_ORG}/#{GITHUB_REPO}/master/properties/defaults.properties"
+PROPERTIES_PATH = params["prop_path"]
+GITHUB_PROPERTIES = "#{GITHUB_URL}/#{GITHUB_ORG}/#{GITHUB_REPO}/master/#{PROPERTIES_PATH}"
 puts "properties at #{GITHUB_PROPERTIES}<hr>"
 demo = Includer.new(
 	GITHUB_URL,
