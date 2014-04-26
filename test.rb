83992f60246a09b6a571c5f1e5943cededa37f4c
Gengar003 <gengar003@gmail.com>
2014-04-26 02:31:14 -0500

    [fixed] extra /

--- test.rb
+++ test.rb
@@ -12,7 +12,7 @@ params = Rack::Utils.parse_nested_query( ENV["QUERY_STRING"] )
 GITHUB_URL = "https://raw.githubusercontent.com"
 GITHUB_ORG = params["org"]
 GITHUB_REPO = params["repo"]
-GITHUB_SRCROOT = "/src"
+GITHUB_SRCROOT = "src"
 GITHUB_PROPERTIES = "#{GITHUB_URL}/#{GITHUB_ORG}/#{GITHUB_REPO}/master/properties/defaults.properties"
 puts "properties at #{GITHUB_PROPERTIES}<hr>"
 demo = Includer.new(
